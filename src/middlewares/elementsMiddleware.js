0c627a9f7b67ee88214ebee84cc32565cf4db328
Timothe <timothe.perigois@gmail.com>
2023-02-01 12:44:53 +0100

    change of the api address for a local file following the closing of the server

--- src/middlewares/elementsMiddleware.js
+++ src/middlewares/elementsMiddleware.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 import { GET_ALL_ELEMENTS_FROM_API, setAllElementsInState } from '../actions/periodicTable';
 import { setLoad } from '../actions/loader';
+import data from 'src/datas/api-allElements.json';
+
 const elementsMiddleware = (store) => (next) => (action) => {
 
     if (action.type === GET_ALL_ELEMENTS_FROM_API){
 
-        axios.get('https://periodic-table-elements-info.herokuapp.com/elements')
+        axios.get('/api-allElements.json')
 
         .then((response) => {
        
